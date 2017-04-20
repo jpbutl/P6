fc63aeaa9b1a9f6ea984fee7ce095a2480392764
JULIAN BUTLER <jpbutler@10-150-44-193.dhcp-m.deakin.edu.au>
2017-04-20 18:00:58 +1000

    4th commit

---
+++ Prac6/Prac6/Trip.swift
@@ -0,0 +1,56 @@
+//
+//  Trip.swift
+//  Prac6
+//
+//  Created by JULIAN BUTLER on 20/04/2017.
+//  Copyright © 2017 JULIAN BUTLER. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Trip
+{
+    var tripDate : Date
+    var tripDuration : Int
+    var tripDestination : String
+    var destinationImage : UIImage
+    
+    init(date : Date, duration : Int, destination: String, img : UIImage)
+    {
+        self.tripDate = date
+        self.tripDuration = duration
+        self.tripDestination = destination
+        self.destinationImage = img
+    }
+}
+
+
+class Utilities
+{
+    static var dateFormatter = DateFormatter()
+    static var trips : [Trip] = []
+    static func loadTrips()
+    {
+        dateFormatter.dateFormat = "yy/mm/dd"
+        trips =
+        [
+            Trip(date : dateFormatter.date(from: "17/04/24")!, duration : 2, destination : "Melbourne" , img : UIImage(named: "melbourne.jpg")!),
+            Trip(date : dateFormatter.date(from: "17/05/24")!, duration : 2, destination : "Sydney", img : UIImage(named: "sydney.jpeg")!)
+        ]
+        //refer to file as .jpg/.jpeg's
+    }
+}
+
+//19. do it here?
+
+//override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//    performSegue(withIdentifier: "segueTripDetails", sender: Utilities.trips[indexPath.row])
+//}
+//
+//override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//    //
+//    if segue.identifier == "segueTripDetails" {
+//        (segue.destination as! TblViewControllerTripInfo).trip = (sender as! Trip)
+//    }
+//}
